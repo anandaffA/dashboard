252757991f308ce880b1c4eeb5453d337fc24ad6
anandaffaA <anandaffa.apriadi@gmail.com>
2024-11-15 17:14:12 +0700

    2

--- test1/urls.py
+++ test1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('photos/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('photos/', views.get_images, name='get_images'),
 ]
--- test1/views.py
+++ test1/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from urllib.parse import urlparse, parse_qs
 from django.http import JsonResponse
 from django.core.paginator import Paginator
 import requests
@@ -8,25 +9,45 @@ def index(request):
     url = 'index.html'
     return render(request, url)
 
-
 def dashboard(request):
-    url = "https://picsum.photos/v2/list"
-    # url = "https://picsum.photos/v2/list?page=2&limit=1"
-    response = requests.get(url)
+    url = 'dashboard.html'
+    return render(request, url)
 
-    if response.status_code == 200:
-        users = response.json()
-        # Get page number from request (default is page 1)
-        page_number = request.GET.get('page', 1)
-        paginator = Paginator(users, 10)  # Show 10 users per page
-        page = paginator.get_page(page_number)
+def get_images(request):
+    url = 'https://picsum.photos/v2/list'
+    params = {
+        'page': request.GET.get('page', 1),
+        'limit': request.GET.get('limit', 9),
+    }
+    response = requests.get(url, params=params)
 
-        # Return paginated data as JSON
-        return JsonResponse({
-            'users': list(page),
-            'total_pages': paginator.num_pages,
-            'current_page': page.number
-        })
+    # Check the Link header
+    link_header = response.headers.get('Link')
+    images = response.json()
+
+    # Optionally, parse the Link header if needed
+    pagination_links = parse_link_header(link_header) if link_header else {}
+    def extract_page_from_url(full_url):
+        parsed_url = urlparse(full_url)
+        query_params = parse_qs(parsed_url.query)
+        page = query_params.get('page', ['1'])[0]  # Default to '1' if not found
+        limit = query_params.get('limit', [params["limit"]])[0]  # Use current limit if not found
+        return f'/photos?page={page}&limit={limit}'
+        
+    next_page_url = extract_page_from_url(pagination_links.get("next")) if pagination_links.get('next') else None
+
+    return JsonResponse({
+        'images': images,
+        'pagination': next_page_url,
+    })
+
     
-    else :
-        return JsonResponse({"error": "Failed to fetch data"}, status=500)
+def parse_link_header(header):
+    links = {}
+    parts = header.split(', ')
+    for part in parts:
+        section = part.split('; ')
+        url = section[0].replace('<', '').replace('>', '')
+        rel = section[1].replace('rel="', '').replace('"', '')
+        links[rel] = url
+    return links
